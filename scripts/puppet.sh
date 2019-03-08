b1c767c370689aea53103fff78978cb251a4fcda
redwiz666 <41351318+redwiz666@users.noreply.github.com>
2019-03-08 12:02:08 -0600

    Update puppet.sh

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -10,6 +10,14 @@ wget "https://apt.puppet.com/puppet5-release-$os_release.deb"
 dpkg -i "puppet5-release-$os_release.deb"
 apt-get -q update
 
+#Setup masterless mode
+echo "[main]
+logdir=/var/log/puppet
+vardir=/var/lib/puppet
+ssldir=/var/lib/puppet/ssl
+rundir=/var/run/puppet
+modulepath=/etc/puppet/code/modules" > /etc/puppet/puppet.conf
+
 #set puppet to auto start
 echo "Setting puppet to start on reboot"
 puppet resource service puppet ensure=running enable=true
